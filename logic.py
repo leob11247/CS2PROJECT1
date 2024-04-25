42ff8a2a7e53ece13307c658741caf2510f21889
leob11247 <157445941+leob11247@users.noreply.github.com>
2024-04-24 23:51:14 -0500

    pyqt

--- logic.py
+++ logic.py
@@ -1,1 +1,9 @@
 from gui import *
+from PyQt6.QtWidgets import *
+from gui import *
+
+class Logic(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
